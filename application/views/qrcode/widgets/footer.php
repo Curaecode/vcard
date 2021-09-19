674680d648560b161b7e31fbb568f81b986011ae
Haroon Abbas <walkin.logic@gmail.com>
2021-09-19 11:26:56 +0500

    #update

--- application/views/qrcode/widgets/footer.php
+++ application/views/qrcode/widgets/footer.php
@@ -4,7 +4,7 @@
 	  <script src="<?php echo base_url();?>resources/assets/js/popper.js"></script>
 	  <script src="<?php echo base_url();?>resources/assets/js/bootstrap.min.js"></script>
 	  <script src="<?php echo base_url();?>resources/assets/js/bootstrap-datepicker.min.js"></script>
-	  <script src="<?php echo base_url();?>resources/assets/js/main.js"></script>
+	  <script src="<?php echo base_url();?>resources/assets/js/main.js?v=<?php echo time();?>"></script>
 	  <script src="<?php echo res_url(); ?>codebase/assets/js/plugins/sweetalert2/sweetalert2.min.js"></script>
 	  <script type="text/javascript" src="<?php echo base_url();?>resources/assets/js/jquery.mask.js"></script>
 	   <script>
