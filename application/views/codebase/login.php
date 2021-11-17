2be62070128f78ab41bbe324daf3b16f10a104b3
Haroon Abbas <walkin.logic@gmail.com>
2021-11-17 14:17:22 +0500

    #update login page design

--- application/views/codebase/login.php
+++ application/views/codebase/login.php
@@ -1,126 +1,110 @@
-<!doctype html>
-<html lang="en" class="no-focus">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+<!DOCTYPE html>
+<html dir="ltr">
 
-       <title><?php /* echo getSiteData('siteName')[0]->value; ?> | <?php echo @$title;  */?> CuraeChoice</title>
-
-        
-
-        <!-- Icons -->
-        <!-- The following icons can be replaced with your own, they are used by desktop and mobile browsers -->
-        <link rel="shortcut icon" href="<?php echo res_url(); ?>codebase/assets/media/favicons/favicon.png">
-        <link rel="icon" type="image/png" sizes="192x192" href="<?php echo res_url(); ?>codebase/assets/media/favicons/favicon-192x192.png">
-        <link rel="apple-touch-icon" sizes="180x180" href="<?php echo res_url(); ?>codebase/assets/media/favicons/apple-touch-icon-180x180.png">
-        <!-- END Icons -->
-
-        <!-- Stylesheets -->
-
-        <!-- Fonts and Codebase framework -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Muli:300,400,400i,600,700">
-        <link rel="stylesheet" id="css-main" href="<?php echo res_url(); ?>codebase/assets/css/codebase.min.css">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1"> 
+    <meta name="robots" content="noindex,nofollow">
+    <title>CuraeChoice</title>
+	<link rel="shortcut icon" href="<?php echo res_url(); ?>codebase/assets/media/favicons/favicon.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="<?php echo res_url(); ?>codebase/assets/media/favicons/favicon-192x192.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo res_url(); ?>codebase/assets/media/favicons/apple-touch-icon-180x180.png">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Muli:300,400,400i,600,700">	
+    <link rel="canonical" href="<?php echo base_url();?>" />
+    <!-- Favicon icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo base_url();?>assets//images/favicon.png">
+    <!-- Custom CSS -->
+    <link href="<?php echo base_url();?>assets/css/style.min.css" rel="stylesheet">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+</head>
  
-        <style>
-            .btn.btn-rounded {
-    border-radius: 0px!important;
-}
- 
-.bg-gd-dusk{
-    background: linear-gradient( 
-45deg
- , #19aae2, #e2e546) !important;
-}
-        </style>
-    </head>
-    <body>
-
+<body>
+    <div class="main-wrapper">
         
-        <div id="page-container" class="main-content-boxed">
-
-            <!-- Main Container -->
-            <main id="main-container">
-
-                <!-- Page Content -->
-                <div class="bg-gd-dusk">
-                    <div class="fix_head_fit hero-static content content-full invisible" data-toggle="appear">
-						<div class="bg-white px-20" style="max-width: 400px;margin: 0 auto;min-width: 300px;">
-						   <!-- Header -->
-							<div class="py-30 px-5 text-center">
-								<a class="link-effect font-w700" href="<?php echo admin_url();?>">
-									<img src="<?php echo res_url();?>img/image_logo.png" style="width:116px;margin-top:-9px;"> 
-								</a>
-								<h1 class="h2 font-w700 mt-50 mb-10">Welcome to Your Dashboard</h1>
-								<h2 class="h4 font-w400 text-muted mb-0">Please sign in</h2>
-							</div>
-							<!-- END Header -->
-
-							<!-- Sign In Form -->
-							<div class="row justify-content-center px-5">
-								<div class="col-sm-8 col-md-10 col-xl-10">
-								
-									<?php 
+        <!-- -------------------------------------------------------------- -->
+        <!-- Preloader - style you can find in spinners.css -->
+        <!-- -------------------------------------------------------------- -->
+        <div class="preloader">
+        <svg class="tea lds-ripple" width="37" height="48" viewbox="0 0 37 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M27.0819 17H3.02508C1.91076 17 1.01376 17.9059 1.0485 19.0197C1.15761 22.5177 1.49703 29.7374 2.5 34C4.07125 40.6778 7.18553 44.8868 8.44856 46.3845C8.79051 46.79 9.29799 47 9.82843 47H20.0218C20.639 47 21.2193 46.7159 21.5659 46.2052C22.6765 44.5687 25.2312 40.4282 27.5 34C28.9757 29.8188 29.084 22.4043 29.0441 18.9156C29.0319 17.8436 28.1539 17 27.0819 17Z" stroke="#1e88e5" stroke-width="2"></path>
+          <path d="M29 23.5C29 23.5 34.5 20.5 35.5 25.4999C36.0986 28.4926 34.2033 31.5383 32 32.8713C29.4555 34.4108 28 34 28 34" stroke="#1e88e5" stroke-width="2"></path>
+          <path id="teabag" fill="#1e88e5" fill-rule="evenodd" clip-rule="evenodd" d="M16 25V17H14V25H12C10.3431 25 9 26.3431 9 28V34C9 35.6569 10.3431 37 12 37H18C19.6569 37 21 35.6569 21 34V28C21 26.3431 19.6569 25 18 25H16ZM11 28C11 27.4477 11.4477 27 12 27H18C18.5523 27 19 27.4477 19 28V34C19 34.5523 18.5523 35 18 35H12C11.4477 35 11 34.5523 11 34V28Z"></path>
+          <path id="steamL" d="M17 1C17 1 17 4.5 14 6.5C11 8.5 11 12 11 12" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" stroke="#1e88e5"></path>
+          <path id="steamR" d="M21 6C21 6 21 8.22727 19 9.5C17 10.7727 17 13 17 13" stroke="#1e88e5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+        </svg>
+    </div>
+        <!-- -------------------------------------------------------------- -->
+        <!-- Preloader - style you can find in spinners.css -->
+        <!-- -------------------------------------------------------------- -->
+        <!-- -------------------------------------------------------------- -->
+        <!-- Login box.scss -->
+        <!-- -------------------------------------------------------------- -->
+        <div class="auth-wrapper d-flex no-block justify-content-center align-items-center" style="background:url(../assets/images/background/login-register.jpg) no-repeat center center; background-size: cover;">
+            <div class="auth-box p-4 bg-white rounded">
+                <div id="loginform">
+                    <div class="logo">
+                        <h3 class="box-title mb-3">Sign In</h3>
+                    </div>
+                    <!-- Form -->
+                    <div class="row">
+                        <div class="col-12">
+							<?php 
 									getMsg();
 									echo isset($msg)?$msg:""; 
 									
-									?>
-				
-								   <form class="js-validation-signin" action="" method="post">
-										<div class="form-group row">
-											<div class="col-12">
-												<div class="form-material floating">
-													<input type="text" class="form-control" id="login-username" name="email">
-													<label for="login-username">Username</label>
-												</div>
-											</div>
-										</div>
-										<div class="form-group row">
-											<div class="col-12">
-												<div class="form-material floating">
-													<input type="password" class="form-control" id="login-password" name="password">
-													<label for="login-password">Password</label>
-												</div>
-											</div>
-										</div>
-										<div class="form-group row gutters-tiny">
-											<div class="col-12 mb-10">
-												<button type="submit" class="btn btn-block btn-hero btn-noborder btn-rounded btn-alt-primary">
-													<i class="si si-login mr-10"></i> Sign In
-												</button>
-											</div> 
-										</div>
-									</form>
-								</div>
-							</div>
-							<!-- END Sign In Form -->
-						</div>
-					</div>
+							?> 
+                            <form class="form-horizontal mt-3 form-material js-validation-signin" id="loginform"  action="" method="post">
+                                <div class="form-group mb-3">
+                                    <div class="">
+                                        <input class="form-control" type="text" name="email" required="" placeholder="Username"> </div>
+                                </div>
+                                <div class="form-group mb-4">
+                                    <div class="">
+                                        <input class="form-control" type="password" name="password" required="" placeholder="Password"> </div>
+                                </div> 
+                                <div class="form-group text-center mt-4 mb-3">
+                                    <div class="col-xs-12">
+                                        <button class="btn btn-info d-block w-100 waves-effect waves-light" type="submit">Log In</button>
+                                    </div>
+                                </div>
+                                
+                            </form>
+                        </div>
+                    </div>
                 </div>
-                <!-- END Page Content -->
-
-            </main>
-            <!-- END Main Container -->
+                
+            </div>
         </div>
-        <!-- END Page Container -->
+        <!-- -------------------------------------------------------------- -->
+        <!-- Login box.scss -->
+        <!-- -------------------------------------------------------------- -->
+    </div>
+    <!-- -------------------------------------------------------------- -->
+    <!-- All Required js -->
+    <!-- -------------------------------------------------------------- -->
+    <script src="../assets/libs/jquery/dist/jquery.min.js"></script>
+    <!-- Bootstrap tether Core JavaScript -->
+    <script src="../assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- -------------------------------------------------------------- -->
+    <!-- This page plugin js -->
+    <!-- -------------------------------------------------------------- -->
+    
+    <script>
+        $(".preloader").fadeOut();
+    // ==============================================================
+    // Login and Recover Password
+    // ==============================================================
+    $('#to-recover').on("click", function() {
+        $("#loginform").slideUp();
+        $("#recoverform").fadeIn();
+    });
+    </script>
+</body>
 
-      
-        <script src="<?php echo res_url(); ?>codebase/assets/js/codebase.core.min.js"></script>
- 
-        <script src="<?php echo res_url(); ?>codebase/assets/js/codebase.app.min.js"></script>
-
-        <!-- Page JS Plugins -->
-        <script src="<?php echo res_url(); ?>codebase/assets/js/plugins/jquery-validation/jquery.validate.min.js"></script>
-
-        <!-- Page JS Code -->
-        <script src="<?php echo res_url(); ?>codebase/assets/js/pages/op_auth_signin.min.js"></script>
-		<!-- Global site tag (gtag.js) - Google Analytics -->
-		<script async src="https://www.googletagmanager.com/gtag/js?id=G-G9E7HV7G7R"></script>
-		<script>
-		  window.dataLayer = window.dataLayer || [];
-		  function gtag(){dataLayer.push(arguments);}
-		  gtag('js', new Date());
-
-		  gtag('config', 'G-G9E7HV7G7R');
-		</script>
-    </body>
 </html>
