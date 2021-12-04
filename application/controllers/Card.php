d67abb02909aa89dfdbeba196fe3aa625d292942
Haroon Abbas <walkin.logic@gmail.com>
2021-12-04 19:37:18 +0500

    #update vcard in png

--- application/controllers/Card.php
+++ application/controllers/Card.php
@@ -5,23 +5,41 @@ class Card extends CI_Controller{
 		parent::__construct();
 		$this->load->model("admin/model");
 	} 
-	function index($id=1){
-		$data['showname']=$this->model->getDatarow("config","where isVisible=1 AND name='showname'"); 
-		$data['showdependent']=$this->model->getDatarow("config","where isVisible=1 AND name='showdependent'"); 
-		$data['image']=$this->model->getDatarow("config","where isVisible=1 AND name='image'"); 
+	function index($id=0){ 
+		if($id>0){
+			$this->load->helper('phantomjs');
+			$this->load->helper('url');
+			$filename="cc_ex_".md5($id).".png";
+			$url =  base_url()."card/design/$id/".md5($id);
+			$filenamepath="/pngfiles/".$filename;
+			
+			if(!rasterize_wrapper($url,  dirname(dirname(dirname(__FILE__))).$filenamepath)){
+				echo  'PNG Generation failed';
+				exit;
+			} 
+		}
 		 
-		$last_data=$this->model->getLastData2("contacts",$id);
-		$company_id= $last_data->company_id;
-		$contract_number= $last_data->contract_number;
-		$data['dependent']=$this->model->getdependents("contact_dependant",$contract_number);
-		$data['contact']=$last_data;
 		
-		$this->db->select('*');
-		$this->db->where('is_card',1);
-		$query = $this->db->get( 'care_coordination' );
-		$data['providers'] = $query->result();
-		 
-		 $this->load->view('card/index',$data);
+    } 
+	function design($id=0,$enid=0){
+		if(md5($id) == $enid){
+			$data['showname']=$this->model->getDatarow("config","where isVisible=1 AND name='showname'"); 
+			$data['showdependent']=$this->model->getDatarow("config","where isVisible=1 AND name='showdependent'"); 
+			$data['image']=$this->model->getDatarow("config","where isVisible=1 AND name='image'"); 
+			 
+			$last_data=$this->model->getLastData2("contacts",$id);
+			$company_id= $last_data->company_id;
+			$contract_number= $last_data->contract_number;
+			$data['dependent']=$this->model->getdependents("contact_dependant",$contract_number);
+			$data['contact']=$last_data;
+			
+			$this->db->select('*');
+			$this->db->where('is_card',1);
+			$query = $this->db->get( 'care_coordination' );
+			$data['providers'] = $query->result(); 
+			$this->load->view('card/indexpng',$data);
+			
+		} 
     }
 }
 ?>
