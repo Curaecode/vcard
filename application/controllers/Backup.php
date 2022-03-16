7e5377ce53fcee86f8cca74c8a2cf4abff1987d7
Haroon <walkin.logic@gmail.com>
2022-03-16 13:22:16 +0500

    #update

--- application/controllers/Backup.php
+++ application/controllers/Backup.php
@@ -8,29 +8,34 @@ class Backup extends CI_Controller {
 	}
 	public function index(){
 			ini_set('memory_limit', '-1');
-			
+		$myfile_path = dirname(dirname(dirname(__FILE__))).'/resources/backup/';
+		
 		$dbhost = $this->db->hostname;
 		$dbuser = $this->db->username;
 		$dbpass = $this->db->password;
 		$dbname = $this->db->database;
 		$mysqldump=exec('which mysqldump');
 		$time=date('Y-m-d_H-i');
-		$myfile_path = dirname(dirname(dirname(__FILE__))).'/resources/backup/';
+		
 		$command = "$mysqldump --opt -h $dbhost -u $dbuser -p$dbpass $dbname > $myfile_path".$dbname."_backup_".$time.".sql";
 		exec($command);
 		$zipcommand="zip --junk-paths ".$myfile_path."BD-backup_".$time.".zip  ".$myfile_path.$dbname."_backup_".$time.".sql";
 		exec($zipcommand);
 		 
-		unlink($myfile_path.$dbname."_backup_".$time.".sql");  	
-			
-			/* $this->load->dbutil();
-			$prefs = array('format' => 'zip', 'filename' => 'Database-backup_' . date('Y-m-d_H-i'));
-			$backup = $this->dbutil->backup($prefs);
+		unlink($myfile_path.$dbname."_backup_".$time.".sql");    
 		
-		if (!write_file('./resources/backup/BD-backup_' . date('Y-m-d_H-i') . '.zip', $backup)) {
-			 
-		}else {
-			 
-		} */
+		$path = $myfile_path;
+		if ($handle = opendir($path)) {
+			while (false !== ($filename = readdir($handle))) {
+				$filelastmodified = filemtime($path . $filename);
+				if((time() - $filelastmodified) > 2592000)
+				{
+					if($filename != '..' && $filename != '.htaccess'){
+						 unlink($path . $filename); 
+					} 
+				}
+			}
+			closedir($handle);
+		} 
 	}  
 }
